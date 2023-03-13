5dca62d15fb66ffdc0a1c60c4719be19980c218f
nameko710 <118578225+nameko710@users.noreply.github.com>
2023-03-13 09:46:46 +0900

    first commit

---
+++ app/src/main/java/nameko710/android/zyanken/MainActivity.kt
@@ -0,0 +1,11 @@
+package nameko710.android.zyanken
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
