199751acd5e8e6afb5da254e8acfd92f0850faeb
nameko710 <118578225+nameko710@users.noreply.github.com>
2023-03-16 20:13:58 +0900

    結果の画面部品を作成

---
+++ app/src/main/java/nameko710/android/zyanken/ResultActivity.kt
@@ -0,0 +1,11 @@
+package nameko710.android.zyanken
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
